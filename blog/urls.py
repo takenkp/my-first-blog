4deffb8bcd1fb72ecbcb69358b0b02b37cac47de
kproh99 <kproh99@naver.com>
2019-02-10 12:15:52 +0900

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
-] # 문자열이 아무것도 없을때,매칭 // views.post_list
+ # 문자열이 아무것도 없을때,매칭 // views.post_list
   # // name='post_list'url에 이름을 붙여 뷰를 식
+    url(r'^post/(?P<pk>\d+)/$', views.post_detail,name='post_detail')
+]
