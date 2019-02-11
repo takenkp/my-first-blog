4bef9b031d48d453e5cde56f0ceab1990fe9f09a
kproh99 <kproh99@naver.com>
2019-02-11 14:33:25 +0900

    Added views to create/edit blog post inside the site.

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
  # 문자열이 아무것도 없을때,매칭 // views.post_list
   # // name='post_list'url에 이름을 붙여 뷰를 식
-    url(r'^post/(?P<pk>\d+)/$', views.post_detail,name='post_detail')
+    url(r'^post/(?P<pk>\d+)/$', views.post_detail,name='post_detail'),
+    url(r'^post/new/$', views.post_new, name='post_new'),
+    url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
 ]
